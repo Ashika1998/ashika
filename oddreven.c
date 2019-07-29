c5f3e4440cfe9dc48b4b11e5c87f013bb496aaa2
Ashika1998 <52189098+Ashika1998@users.noreply.github.com>
2019-07-29 15:45:17 +0530

    Add files via upload

---
+++ oddreven.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+
+int main() 
+{
+  
+ long int a;
+
+   scanf("%ld",&a);
+
+   if((a%2)==0)
+  
+ {
+  
+     printf("Even");
+
+   }
+
+   else if(a<0)
+ 
+  {
+       
+      printf("invalid");
+ 
+  }
+
+   else
+
+   {
+
+       printf("Odd");
+
+   }
+ 
+  return 0;
+}
